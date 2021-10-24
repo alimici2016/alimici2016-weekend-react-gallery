e625e07702efc5b3c01921867642b1491e74206d
alimici2016 <78386559+alimici2016@users.noreply.github.com>
2021-10-24 15:13:46 -0500

    Update gallery.data.js

--- server/modules/gallery.data.js
+++ server/modules/gallery.data.js
@@ -1,5 +1,8 @@
 const galleryItems = [
-    { id: 1, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 }
+    { id: 1, path: 'images/raisingInTheSun.jpg', description: 'Official cover photo for the film A Raising in the Sun.', likes: 0 },
+    { id: 2, path: 'images/45years.jpg', description: 'Official cover photo for the film 45 years.', likes: 0},
+    { id: 3, path: 'images/coldWar.jpg', description: 'Official cover photo for the film Cold War.', likes: 0},
+    { id: 4, path: 'images/ordinaryPeople.jpg', description: 'Official cover photo for the film Ordinary people.', likes: 0}
 ];
 
 module.exports = galleryItems;
